272573766cfc9abf21031d811c7f250651ab5818
nirmal-codebuster <nirmal@codebuster.io>
2023-12-14 16:03:08 +0530

    uses reference in collection

---
+++ models/Leave.js
@@ -0,0 +1,22 @@
+// models/Leave.js
+const mongoose = require("mongoose");
+
+const leaveSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  department: { type: String, required: true },
+  leaveType: {
+    type: String,
+    enum: ["Casual Leave", "Sick Leave"],
+    required: true,
+  },
+  from_date: { type: Date, required: true },
+  to_date: { type: Date, required: true },
+  number_of_days: { type: Number, required: true },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Leave", leaveSchema);
